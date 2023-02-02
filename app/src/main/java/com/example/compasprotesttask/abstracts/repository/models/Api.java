4b86f0cbb31d391a833791d3929545a9f9e9c956
Meloy9992 <81082136+Meloy9992@users.noreply.github.com>
2023-02-02 05:46:05 +0300

    Update Api.java

--- app/src/main/java/com/example/compasprotesttask/abstracts/repository/models/Api.java
+++ app/src/main/java/com/example/compasprotesttask/abstracts/repository/models/Api.java
@@ -1,7 +1,7 @@
 package com.example.compasprotesttask.abstracts.repository.models;
 
 public class Api {
-    private final String API_KEY = "21d93414a9c73d83ab440d2f8b4d3c79";
+    private final String API_KEY = ""; \\Ваш API ключ
 
     public String getAPI_KEY() {
         return API_KEY;
